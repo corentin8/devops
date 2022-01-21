74fb3458ade8fe6b9364440c0837125c98beacfc
Besson.Corentin <58816557+corentin8@users.noreply.github.com>
2022-01-21 20:18:27 +0100

    fallback

--- fight/src/main/java/me/escoffier/workshop/fight/HeroClient.java
+++ fight/src/main/java/me/escoffier/workshop/fight/HeroClient.java
@@ -1,5 +1,6 @@
 package me.escoffier.workshop.fight;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import fight.src.main.java.me.escoffier.workshop.hero.Hero;
@@ -14,8 +15,25 @@ import java.time.temporal.ChronoUnit;
 @RegisterRestClient(configKey = "hero")
 public interface HeroClient {
     
+
+    @Fallback(fallbackMethod = "getFallbackHero")
     @Path("/heroes/random")
     @GET
     Hero findRandom();
 
+
+
+    //VALUES (nextval('hibernate_sequence'), 'Buffy', 'Buffy Anne Summers', 'https://www.superherodb.com/pictures2/portraits/10/050/10430.jpg', 
+    //'Accelerated Healing, Agility, Danger Sense, Durability, Enhanced Memory, Psionic Powers, Reflexes, Stamina, Stealth, Super Speed, Super Strength, Vision - Telescopic', 6);
+
+
+    default Hero getFallbackHero() {
+        Hero h = new Hero();
+        h.name="Buffy";
+        h.otherName="Buffy Anne Summers";
+        h.level=6;
+        h.picture="https://www.superherodb.com/pictures2/portraits/10/050/10430.jpg";
+        h.powers="Accelerated Healing, Agility, Danger Sense, Durability, Enhanced Memory, Psionic Powers, Reflexes, Stamina, Stealth, Super Speed, Super Strength, Vision - Telescopic";
+        return h;
+    }
 }
--- fight/src/main/java/me/escoffier/workshop/fight/VilainClient.java
+++ fight/src/main/java/me/escoffier/workshop/fight/VilainClient.java
@@ -1,5 +1,6 @@
 package me.escoffier.workshop.fight;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import fight.src.main.java.me.escoffier.workshop.vilain.Villain;
@@ -13,8 +14,20 @@ import java.time.temporal.ChronoUnit;
 @RegisterRestClient(configKey = "vilain")
 public interface VilainClient {
 
+    @Fallback(fallbackMethod = "getFallbackVilain")
     @Path("/vilain/random")
     @GET
     Villain findRandom();
-    
+
+
+    default Villain getFallbackVilain() {
+        Villain v = new Villain();
+        v.name="Blackwing";
+        v.otherName="Joseph Manfredi";
+        v.level=3;
+        v.picture="https://www.superherodb.com/pictures2/portraits/10/050/249.jpg";
+        v.powers="Flight, Telepathy";
+        return v;
+    }
+
 }
